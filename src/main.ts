784ca6f19e4158eb8dbdb4da4175f2f9829f394e
Will Schaller <willschaller@gmail.com>
2022-04-27 20:42:31 +0200

    wip

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import Timer from "timer";
 import Digital from "pins/digital";
 import SMBus from "pins/smbus";
 
-class TrackBall {
+class Trackball {
   static constants = {
     I2C_ADDR_PRIMARY: 0x0a,
     I2C_ADDR_ALTERNATIVE: 0x0b,
@@ -38,34 +38,31 @@ class TrackBall {
   i2c_address: number;
   i2c_bus: SMBus;
   timeout: number;
-  constructor(
-    address = TrackBall.constants.I2C_ADDR_PRIMARY,
-    i2c_bus = 1,
-    timeout = 5
-  ) {
+  constructor(address = Trackball.constants.I2C_ADDR_PRIMARY, timeout = 5) {
     this.i2c_address = address;
     this.i2c_bus = new SMBus({ sda: 23, scl: 22, address, hz: 250_000 });
     this.timeout = timeout;
-    trace("SMBus initialized");
-    if (this.chipId !== TrackBall.constants.CHIP_ID) {
-      throw new Error("Chip ID does not match Pimoroni TrackBall chip ID");
+    trace("\nTrackball initialized");
+    if (this.chipId !== Trackball.constants.CHIP_ID) {
+      throw new Error("Chip ID does not match Pimoroni Trackball chip ID");
     }
   }
   private get chipId() {
+    trace("\nReading chip id");
     const uInt8Array = this.i2c_bus.readBlock(
-      TrackBall.constants.REG_CHIP_ID_L,
+      Trackball.constants.REG_CHIP_ID_L,
       2
     );
     return new DataView(uInt8Array.buffer, 0).getUint16(0, true);
   }
   readValues() {
-    const values = this.i2c_bus.readBlock(TrackBall.constants.REG_LEFT, 5);
+    const values = this.i2c_bus.readBlock(Trackball.constants.REG_LEFT, 5);
     return {
       left: values[0],
       right: values[1],
       up: values[2],
       down: values[3],
-      clicked: values[4] === TrackBall.constants.MSK_CLICKED,
+      clicked: values[4] === Trackball.constants.MSK_CLICKED,
     };
   }
 
@@ -75,17 +72,18 @@ class TrackBall {
     green: number;
     white: number;
   }) {
-    this.i2c_bus.writeByte(TrackBall.constants.REG_LED_RED, values.red);
-    this.i2c_bus.writeByte(TrackBall.constants.REG_LED_BLU, values.blue);
-    this.i2c_bus.writeByte(TrackBall.constants.REG_LED_GRN, values.green);
-    this.i2c_bus.writeByte(TrackBall.constants.REG_LED_WHT, values.white);
+    this.i2c_bus.writeByte(Trackball.constants.REG_LED_RED, values.red);
+    this.i2c_bus.writeByte(Trackball.constants.REG_LED_BLU, values.blue);
+    this.i2c_bus.writeByte(Trackball.constants.REG_LED_GRN, values.green);
+    this.i2c_bus.writeByte(Trackball.constants.REG_LED_WHT, values.white);
   }
 }
 
 let lightOn = false;
 trace("Run");
-const trackball = new TrackBall();
+const trackball = new Trackball();
 Timer.repeat(() => {
   Digital.write(13, lightOn ? 1 : 0);
   lightOn = !lightOn;
+  // trace(JSON.stringify(trackball.readValues()));
 }, 500);
